286c4f0fdb1075adaabc9ced6d040b5fd6f4ce1c
CandelaNiz98 <candeniz193@gmail.com>
2022-05-22 23:59:16 -0300

    first commit

---
+++ src/app/heroform/heroform.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, MinLengthValidator, Validators } from '@angular/forms';
+import { Hero } from '../models/hero';
+
+@Component({
+  selector: 'app-heroform',
+  templateUrl: './heroform.component.html',
+  styleUrls: ['./heroform.component.scss']
+})
+export class HeroformComponent implements OnInit {
+  powers=['Laser Eyes']
+  model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
+  submitted = false;
+  heroForm!:FormGroup;
+  constructor(
+    private fb:FormBuilder
+  ) { }
+
+  ngOnInit(){
+    this.heroForm=this.fb.group({
+      nombre:['', Validators.required],
+      password:['', [Validators.required, Validators.minLength(8)]]
+    })
+  }
+  onSubmit() { console.log(this.heroForm.value) }
+
+  newHero() {
+    this.model = new Hero(42, '', '');
+  }
+  /*skyDog(): Hero {
+    const myHero =  new Hero(42, 'SkyDog',
+                           'Fetch any object at any distance',
+                           'Leslie Rollover');
+    console.log('My hero is called ' + myHero.name); // "My hero is called SkyDog"
+    return myHero;
+  }*/
+
+}
